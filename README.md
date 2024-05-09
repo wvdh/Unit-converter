55913322ecf47b9d84d14e79f8dd0a65d5f51197
wvdh <wanda@vandenhoogen.eu>
2024-05-09 02:00:04 +0200

    updates

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Unit-converter
-<br>
 Metric/Imperial Unit Conversion <br>
 <br>
 https://wvdh.github.io/Unit-converter/
 <br>
+<br>
+
+![alt text](image.png)
+
+
 <br>
 1 meter = 3.281 feet<br>
 1 liter = 0.264 gallon<br>
--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-//Function for max input to convert 999.
 function limitToMax(element) {
     if (parseInt(element.value) > 999) {
         element.value = '999';
     }
 }
 
-const inputEl = document.getElementById("input-el")
+let inputValue = 0
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
 const convertBtn = document.getElementById("convert-btn")
 
-//lengthConvert
-//volumeConvert
-//massConvert
+convertBtn.addEventListener("click", () => {
+    inputValue = document.getElementById('input-el').value
+  
+    let meters = (inputValue * 0.3048).toFixed(3)
+    let gallons = (inputValue * 0.2642).toFixed(3)
+    let pounds = (inputValue / 0.45359237).toFixed(3)
+    
+    let feet = (inputValue * 3.2808).toFixed(3)
+    let liters = (inputValue * 3.7854).toFixed(3)
+    let kilos = (inputValue * 0.45359237).toFixed(3)
+  
+    lengthEl.innerText = `${inputValue} meters = ${feet} feet | ${inputValue} feet = ${meters} meters`
+    volumeEl.innerText = `${inputValue} liters = ${gallons} gallons | ${inputValue} gallons = ${liters} liters`
+    massEl.innerText   = `${inputValue} kilos = ${pounds} pounds | ${inputValue} pounds = ${kilos} kilos`
+  })
