02ffb244cef4a8efb841572831a2ac63c53baf46
wvdh <wanda@vandenhoogen.eu>
2024-05-09 15:07:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,15 +12,14 @@ const convertBtn = document.getElementById("convert-btn")
 
 convertBtn.addEventListener("click", () => {
     inputValue = document.getElementById('input-el').value
-  
-    let meters = (inputValue * 0.3048).toFixed(3)
-    let gallons = (inputValue * 0.2642).toFixed(3)
+
+    let feet = (inputValue / 0.3048).toFixed(3)
+    let gallons = (inputValue / 3.7854).toFixed(3)
     let pounds = (inputValue / 0.45359237).toFixed(3)
-    
-    let feet = (inputValue * 3.2808).toFixed(3)
+    let meters = (inputValue * 0.3048).toFixed(3)
     let liters = (inputValue * 3.7854).toFixed(3)
-    let kilos = (inputValue * 0.45359237).toFixed(3)
-  
+    let kilos = (inputValue * 0.45359237).toFixed(3)  
+    
     lengthEl.innerText = `${inputValue} meters = ${feet} feet | ${inputValue} feet = ${meters} meters`
     volumeEl.innerText = `${inputValue} liters = ${gallons} gallons | ${inputValue} gallons = ${liters} liters`
     massEl.innerText   = `${inputValue} kilos = ${pounds} pounds | ${inputValue} pounds = ${kilos} kilos`
